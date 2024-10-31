8321bec3d8e73acc57cc96befbaef89b72e75f79
nrethans <145207516+nrethans@users.noreply.github.com>
2024-10-30 23:05:55 -0500

    threads

--- Laptop_Side/maincode/threads.py
+++ Laptop_Side/maincode/threads.py
@@ -1,6 +1,7 @@
 # Import Classes
 from sendcontrols import Send_Control_Data
 from gyroscope import Gyroscope
+
 # Import Threading
 import threading
 
@@ -8,13 +9,16 @@ import threading
 print("Initializing components...")
 send_control = Send_Control_Data()
 gyroscope    = Gyroscope()
+receiver     = ()
 
 # Start threads for each component
 print("Configuring threads...")
 control_thread   = threading.Thread(target=send_control.start_server)
 gyroscope_thread = threading.Thread(target=gyroscope.RunGyro)
+receiver_thread  = threading.Thread(target=receiver.)
 
 # Start and manage threads
 print("Starting threads...")
 control_thread.start()
 gyroscope_thread.start()
+receiver_thread.start()
